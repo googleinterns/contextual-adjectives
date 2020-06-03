d6df2ef84e24ad924ec930aa2013302f6b359e02
Shashij <shashijgupta@google.com>
2020-06-03 11:07:27 +0530

    Some correction in adj_idf_gen

---
+++ adj_idf_gen.py
@@ -0,0 +1,20 @@
+from nltk.text import TextCollection, Text
+from nltk.corpus import wordnet
+import pickle
+f = open("all.txt", "r")
+lines = f.readlines()
+tokens = []
+for line in lines:
+	line = line.lower()
+	line = line.split("\n")[0].split(" ")
+	tokens.extend(line)
+text = Text(tokens)
+mytexts = TextCollection(text)
+# Print the IDF of a word
+adj_idf = {}
+for synset in list(wordnet.all_synsets(wordnet.ADJ)):
+	adj = synset.lemmas()[0].name().lower()
+	adj_idf[adj] = mytexts.idf(adj)
+
+with open("adj_idf.dat", 'wb') as f:
+	pickle.dump(adj_idf, f)
--- filter_by_idf.py
+++ filter_by_idf.py
@@ -1,19 +1,3 @@
-from nltk.text import TextCollection, Text
-from nltk.corpus import wordnet
-f = open("all.txt", "r")
-lines = f.readlines()
-tokens = []
-for line in lines:
-	line = line.lower()
-	line = line.split("\n")[0].split(" ")
-	tokens.extend(line)
-text = Text(tokens)
-mytexts = TextCollection(text)
-# Print the IDF of a word
-adj_idf = {}
-for synset in list(wordnet.all_synsets(wordnet.ADJ)):
-	adj = synset.lemmas()[0].name().lower()
-	adj_idf[adj] = mytexts.idf(adj)
-
-with open("adj_idf.dat", 'wb') as f:
-	pickle.dump(adj_idf, f)
+import pickle
+with open("adj_idf.dat", 'rb') as f:
+	adj_idf = pickle.load(f)
