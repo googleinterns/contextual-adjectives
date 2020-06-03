078c4b212d91edcaac40088f19ccdd8dcbe082ac
Shashij <shashijgupta@google.com>
2020-06-03 16:17:43 +0530

    Top 500 adjectives with score (IDF)

--- filter_by_idf.py
+++ filter_by_idf.py
@@ -1,3 +1,17 @@
 import pickle
 with open("adj_idf.dat", 'rb') as f:
 	adj_idf = pickle.load(f)
+
+
+# threshold = 8
+# count = 0
+# for adj in adj_idf.keys():
+# 	if adj_idf[adj] < threshold and adj_idf[adj]!=0:
+# 		print(adj)
+# 		count += 1
+
+
+
+sorted_adjs = sorted(adj_idf.items(), key = lambda kv:(kv[1], kv[0]))[:500]
+for adj,val in sorted_adjs:
+	print(adj, val)
