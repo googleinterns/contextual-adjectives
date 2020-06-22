eb72e5f1b4cd29d64db52c74c824e11bb46c0d1d
Shashij <shashijgupta@google.com>
2020-06-22 14:48:18 +0530

    Comments Resolved

--- categorize_adjectives/keywords.py
+++ categorize_adjectives/keywords.py
@@ -2,81 +2,90 @@
 import os
 from nltk.corpus import wordnet as wn
 
-# Folder where generated files are stored
-generated_file = os.path.join(os.getcwd(), '..', 'generated_files/')
+def find_adjectives(adjectives, categories, keywords):
+"""For each adjective, finding similar adjectives and adding to respective categories"""
+	for adjective in adjectives:
+	    definition = adjective.definition().lower()
+	    definition = definition.split(" ")
+	    adj_name = adjective.lemmas()[0].name().lower()
+	    similar_synset = wn.synset(adjective.name()).similar_tos()
+	    similar_adj = []
+	    similar_adj.append(adj_name)
+	    # Finding similar adjectives
+	    for syn in similar_synset:
+	        for lemma in syn.lemmas():
+	            name = lemma.name().lower()
+	            similar_adj.append(name)
+	            if lemma.antonyms():
+	                similar_adj.append(lemma.antonyms()[0].name())
+	    # Finding Categories to which all these adjs should belong
+	    selected_cat = []
+	    for category, category_keywords in keywords.items():
+	        for keyword in category_keywords:
+	            if keyword in definition:
+	                selected_cat.append(category)
+	                continue
+	    for category in selected_cat:
+	        categories[category].extend(similar_adj)
 
-# Fetching all adjective synsets from Wordnet
-adjectives = wn.all_synsets('a')
+	# Removing duplicates from categories
+	comp_set = set()
+	for category in categories:
+	    categories[category] = list(set(categories[category]))
+	    comp_set = comp_set | set(categories[category])
+	return categories
 
-# Semantic Categories for adjectives
-categories = {'Perceptional' : [], 'Spatial' : [], 'Time' : [], 'Motion' : [], 'Substance' : [],
-              'Weather' : [], 'Body' : [], 'Mood' : [], 'Spirit' : [], 'Behavior' : [],
-              'Social' : [], 'Quantity' : [], 'Relational' : [], 'General' : [], 'Others' : []}
+def save_to_csv(categories):
+	"""Saving categories in a CSV file"""
+	f = open(generated_file + "keyword_clusters.csv", "w")
+	for category, adjectives in categories.items():
+	    sentence = category + ", "
+	    for adj in adjectives:
+	        sentence += adj + ","
+	    sentence = sentence[:-1]
+	    sentence += "\n"
+	    f.write(sentence)
+	f.close()
 
-# Keywords used to lookout for adjectives
-keywords = {}
-keywords['Perceptional'] = ['perceptional', 'lightness', 'color', 'sound', 'taste', 'smell',
-                            'surface']
-keywords['Spatial'] = ['spatial', 'dimension', 'direction', 'localization', 'origin', 'form',
-                       'existence']
-keywords['Time'] = ['time', 'velocity', 'age', 'habit']
-keywords['Motion'] = ['motion']
-keywords['Substance'] = ['substance', 'composition', 'state', 'stability', 'consistency',
-                         'ripeness', 'dampness', 'purity', 'gravity', 'weight', 'physics',
-                         'chemistry', 'temperature']
-keywords['Weather'] = ['weather', 'climate']
-keywords['Body'] = ['body', 'life', 'constitution', 'affliction', 'desire', 'sex', 'appearance',
-                    'bodily state']
-keywords['Mood'] = ['feeling', 'stimulus', 'mood']
-keywords['Spirit'] = ['spirit', 'intelligence', 'attention', 'knowledge', 'experience']
-keywords['Behavior'] = ['behavior', 'character', 'animal specific', 'skill', 'relation', 'sympathy',
-                        'inclination']
-keywords['Social'] = ['stratum', 'social', 'institution', 'poltics', 'religion', 'state', 'region']
-keywords['Quantity'] = ['quantity', 'number', 'cost', 'return']
-keywords['Relational'] = ['relational', 'validity', 'certainty', 'requirement', 'effectiveness',
-                          'difficulty', 'energy requirement', 'functioning', 'security', 'order',
-                          'linking', 'correspondence', 'accuracy', 'completeness', 'cause',
-                          'reference', 'beneficial effect']
-keywords['General'] = ['comparative', 'evaluation', 'standard']
+def __name__ == "__main__":
+	# Folder where generated files are stored
+	generated_file = os.path.join(os.getcwd(), '..', 'generated_files/')
 
-# For each adjective, finding similar adjectives and adding to respective categories
-for adjective in adjectives:
-    definition = adjective.definition().lower()
-    definition = definition.split(" ")
-    adj_name = adjective.lemmas()[0].name().lower()
-    similar_synset = wn.synset(adjective.name()).similar_tos()
-    similar_adj = []
-    similar_adj.append(adj_name)
-    # Finding similar adjectives
-    for syn in similar_synset:
-        for lemma in syn.lemmas():
-            name = lemma.name().lower()
-            similar_adj.append(name)
-            if lemma.antonyms():
-                similar_adj.append(lemma.antonyms()[0].name())
-    # Finding Categories to which all these adjs should belong
-    selected_cat = []
-    for category, category_keywords in keywords.items():
-        for keyword in category_keywords:
-            if keyword in definition:
-                selected_cat.append(category)
-                continue
-    for category in selected_cat:
-        categories[category].extend(similar_adj)
+	# Fetching all adjective synsets from Wordnet
+	adjectives = wn.all_synsets('a')
 
-# Removing duplicates from categories
-comp_set = set()
-for category in categories:
-    categories[category] = list(set(categories[category]))
-    comp_set = comp_set | set(categories[category])
+	# Semantic Categories for adjectives
+	categories = {'Perceptional' : [], 'Spatial' : [], 'Time' : [], 'Motion' : [], 'Substance' : [],
+	              'Weather' : [], 'Body' : [], 'Mood' : [], 'Spirit' : [], 'Behavior' : [],
+	              'Social' : [], 'Quantity' : [], 'Relational' : [], 'General' : [], 'Others' : []}
 
-# Saving categories in a CSV file
-f = open(generated_file + "keyword_clusters.csv", "w")
-for category, adjectives in categories.items():
-    sentence = category + ", "
-    for adj in adjectives:
-        sentence += adj + ","
-    sentence = sentence[:-1]
-    sentence += "\n"
-    f.write(sentence)
-f.close()
+	# Keywords used to lookout for adjectives
+	# Categories and keywords taken from 
+	# http://www.sfs.uni-tuebingen.de/projects/ascl/GermaNet/adjectives.shtml
+	keywords = {}
+	keywords['Perceptional'] = ['perceptional', 'lightness', 'color', 'sound', 'taste', 'smell',
+	                            'surface']
+	keywords['Spatial'] = ['spatial', 'dimension', 'direction', 'localization', 'origin', 'form',
+	                       'existence']
+	keywords['Time'] = ['time', 'velocity', 'age', 'habit']
+	keywords['Motion'] = ['motion']
+	keywords['Substance'] = ['substance', 'composition', 'state', 'stability', 'consistency',
+	                         'ripeness', 'dampness', 'purity', 'gravity', 'weight', 'physics',
+	                         'chemistry', 'temperature']
+	keywords['Weather'] = ['weather', 'climate']
+	keywords['Body'] = ['body', 'life', 'constitution', 'affliction', 'desire', 'sex', 'appearance',
+	                    'bodily state']
+	keywords['Mood'] = ['feeling', 'stimulus', 'mood']
+	keywords['Spirit'] = ['spirit', 'intelligence', 'attention', 'knowledge', 'experience']
+	keywords['Behavior'] = ['behavior', 'character', 'animal specific', 'skill', 'relation', 'sympathy',
+	                        'inclination']
+	keywords['Social'] = ['stratum', 'social', 'institution', 'poltics', 'religion', 'state', 'region']
+	keywords['Quantity'] = ['quantity', 'number', 'cost', 'return']
+	keywords['Relational'] = ['relational', 'validity', 'certainty', 'requirement', 'effectiveness',
+	                          'difficulty', 'energy requirement', 'functioning', 'security', 'order',
+	                          'linking', 'correspondence', 'accuracy', 'completeness', 'cause',
+	                          'reference', 'beneficial effect']
+	keywords['General'] = ['comparative', 'evaluation', 'standard']
+
+	categories = find_adjectives(adjectives, categories, keywords)
+	save_to_csv(categories)
--- categorize_adjectives/kmedoids.py
+++ categorize_adjectives/kmedoids.py
@@ -1,5 +1,5 @@
 """Categorize Adjectives using K-medoids algorithm
-Here we only try to categorize most frequent 100 adjectives
+Here we only try to categorize most frequent 150 adjectives
 """
 import os
 import random
@@ -9,59 +9,69 @@ from pyclustering.cluster.kmedoids import kmedoids
 from pyclustering.utils.metric import distance_metric, type_metric
 from libraries.bert import BertSimilarity
 
-# Folder where generated files are stored
-generated_file = os.path.join(os.getcwd(), '..', 'generated_files/')
-
-# Initializing the BERT Similarity function and also storing calculated distances in array.
-BERT = BertSimilarity()
-dis_array = np.zeros((150, 150)) - 1
-
 def distance(w_1, w_2):
     """
-    Calulates and Returns the distance between w1 and w2
+    Calulates and Returns the BERT contexual distance between w1 and w2
     Also stores the distance in dis_array
     """
     if dis_array[w_1, w_2] == -1:
         dis_array[w_1, w_2] = 1-(BERT.get_similarity([(selected_adj[int(w_1)], selected_adj[int(w_2)])],
                                  layer=-2)[0])
         dis_array[w_2, w_1] = dis_array[w_1, w_2]
-    return dis_array[w_1, w_2]
+    return dis_array[w_2, w_1]
 
-# Fetching the noun to adj dictionary from pickle file
-with open(generated_file + 'noun_to_adj_score.dat', 'wb') as f:
-    noun_to_adj = pickle.load(f)
+def get_frequent_adjectives(noun_to_adj, num_adj):
+    """Selecting the most frequent num_adj adjectives"""
+    selected_adj = []
+    adj_count = {}
+    for noun in noun_to_adj.keys():
+        for adj, val in noun_to_adj[noun]:
+            try:
+                adj_count[adj] += -1
+            except KeyError:
+                adj_count[adj] = -1
+    sorted_adjs = sorted(adj_count.items(), key=lambda kv: (kv[1], kv[0]))[:num_adj]
+    for adj, val in sorted_adjs:
+        selected_adj.append(adj)
+    return selected_adj
 
-# Selecting the most frequent 100 adjectives
-NUM_ADJ = 100
-selected_adj = []
-adj_count = {}
-for noun in noun_to_adj.keys():
-    for adj, val in noun_to_adj[noun]:
-        try:
-            adj_count[adj] += -1
-        except KeyError:
-            adj_count[adj] = -1
-sorted_adjs = sorted(adj_count.items(), key=lambda kv: (kv[1], kv[0]))[:NUM_ADJ]
-for adj, val in sorted_adjs:
-    selected_adj.append(adj)
+def kmedoids_clustering(num_adj, num_cluster):
+    """Performs kmedoids clustering"""
+    sample = [np.array(f) for f in range(num_adj)]
+    initial_medoids = random.sample(range(num_adj), num_cluster)
+    metric = distance_metric(type_metric.USER_DEFINED, func=distance)
+    kmedoids_instance = kmedoids(sample, initial_medoids, metric=metric)
+    # run cluster analysis and obtain results
+    kmedoids_instance.process()
+    clusters = kmedoids_instance.get_clusters()
+    return clusters
 
-# Choosing number of Clusters
-NUM_CLUSTER = 10
-sample = [np.array(f) for f in range(NUM_ADJ)]
-initial_medoids = random.sample(range(NUM_ADJ), NUM_CLUSTER)
-metric = distance_metric(type_metric.USER_DEFINED, func=distance)
-kmedoids_instance = kmedoids(sample, initial_medoids, metric=metric)
-# run cluster analysis and obtain results
-kmedoids_instance.process()
-clusters = kmedoids_instance.get_clusters()
+def save_to_csv(clusters):
+    """Save the clusters in a csv file"""
+    f = open(generated_file + "kmedoids_clusters.csv", "w")
+    for cluster in clusters:
+        sentence = ""
+        for idx in cluster:
+            sentence += selected_adj[idx] + ","
+        sentence = sentence[:-1]
+        sentence += "\n"
+        f.write(sentence)
+    f.close()
 
-# Save the clusters in a csv file
-f = open(generated_file + "kmedoids_clusters.csv", "w")
-for cluster in clusters:
-    sentence = ""
-    for idx in cluster:
-        sentence += selected_adj[idx] + ","
-    sentence = sentence[:-1]
-    sentence += "\n"
-    f.write(sentence)
-f.close()
+if __name__ == "__main__":
+    NUM_ADJ = 150
+    NUM_CLUSTER = 10
+
+    # Folder where generated files are stored
+    generated_file = os.path.join(os.getcwd(), '..', 'generated_files/')
+
+    # Initializing the BERT Similarity function and also storing calculated distances in array.
+    BERT = BertSimilarity()
+    dis_array = np.zeros((150, 150)) - 1
+
+    # Fetching the noun to adj dictionary from pickle file
+    with open(generated_file + 'noun_to_adj_score.dat', 'wb') as f:
+        noun_to_adj = pickle.load(f)
+    selected_adj = get_frequent_adjectives(noun_to_adj, NUM_ADJ)
+    clusters = kmedoids_clustering(NUM_ADJ, NUM_CLUSTER)
+    save_to_csv(clusters)
