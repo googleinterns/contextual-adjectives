061461199e5022415399a60ceebd610dbf2e507b
Shashij <shashijgupta@google.com>
2020-06-23 11:26:07 +0530

    added code to calculate bert similarity to most frequent 15000 adjectives

--- bert_similarity_calc.py
+++ bert_similarity_calc.py
@@ -4,6 +4,23 @@ import pickle
 from scipy.spatial.distance import cosine
 adj_list = adjective_list_gen()
 
+with open('noun_to_adj_score.dat', 'wb') as f:
+        noun_to_adj = pickle.load(f)
+
+selected_adj = []
+adj_count = {}
+for noun in noun_to_adj.keys():
+    for adj, val in noun_to_adj[noun]:
+        try:
+            adj_count[adj] += -1
+        except KeyError:
+            adj_count[adj] = -1
+sorted_adjs = sorted(adj_count.items(), key=lambda kv: (kv[1], kv[0]))[:15000]
+for adj, val in sorted_adjs:
+    selected_adj.append(adj)
+
+adj_list = selected_adj
+
 calculated_dis = {}
 x = BertSimilarity()
 tuple_adj = []
